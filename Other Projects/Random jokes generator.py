9d4a1995a2669ba25fb24d586a827f169c246895
Bushra <bushra007891@gmail.com>
2024-10-08 23:38:38 +0530

    Create Random jokes generator.py

---
+++ Other Projects/Random jokes generator.py
@@ -0,0 +1,11 @@
+# Random Jokes Generator
+
+'''Key Features:
+- Generates random jokes using the 'pyjokes' library.
+- Simple and lightweight program for fun and entertainment.'''
+
+# Importing the pyjokes module to fetch jokes
+import pyjokes
+
+# Fetching and printing a random joke
+print(pyjokes.get_joke())
